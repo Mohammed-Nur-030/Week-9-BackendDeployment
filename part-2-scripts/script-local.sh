2b2d2f278d638665dce64dd38ffec5f3825ecc55
Mohammed-Nur-030 <126596779+Mohammed-Nur-030@users.noreply.github.com>
2023-12-29 19:33:09 +0530

    PATH Environment version was v20.5.0 changed to v21.5.0

--- part-2-scripts/script-local.sh
+++ part-2-scripts/script-local.sh
@@ -1,1 +1,1 @@
-ssh -t -i "week-9-app.pem" ubuntu@ec2-3-25-92-34.ap-southeast-2.compute.amazonaws.com	  "source ~/deploy.sh"
+ssh -t -i "week-9-app.pem" ubuntu@ec2-3-104-64-52.ap-southeast-2.compute.amazonaws.com "sudo bash ~/deploy.sh"
--- server/index.js
+++ server/index.js
@@ -11,11 +11,13 @@ app.use(express.json());
 
 app.use("/admin", adminRouter)
 app.use("/user", userRouter)
-app.get("/",(req,res)=>res.json({msg:"Hello Guys"}));
+app.get("/",(req,res)=>res.json(
+    `<h1>HelloWorld</h1>`
+    ));
 
 
 // Connect to MongoDB
 // DONT MISUSE THIS THANKYOU!!
 mongoose.connect('mongodb+srv://xyznur786:aKcPsKhPFL0A3ZDK@cluster0.ao6uktc.mongodb.net/week-9', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "courses" });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(80, () => console.log('Server running on port 3000'));
